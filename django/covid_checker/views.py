79eca06e78b3b287b3b12a9736cde654b73dd40d
Soyeon Jeon <writinghaetae@gmail.com>
2022-05-01 20:59:39 +0900

    modify

--- django/covid_checker/views.py
+++ django/covid_checker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .api import check_covid,mail
-from django.core.cache import cache 
+#from django.core.cache import cache 
 
 def index(request):
 #    cache_data = cache.get("covid_data")
@@ -10,7 +10,7 @@ def index(request):
     mail_data = mail()
     for number in mail_data['tr']['td']:
         num.append(number['span'])
-        num = cache.set("covid_data", num , timeout=600 * 600)
+#        num = cache.set("covid_data", num , timeout=600 * 600)
     return render(request, 'covid_checker/covid_main.html',{'data':data,'today':num})
 
         
